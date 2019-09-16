31bc112227d24fef645835c5c19c49b8d0337424
Alesia Buyalskaya <a.buyalskaya@godeltech.com>
2019-09-16 15:03:06 +0300

    cleanup

--- checkers_bot/checkers_bot/Startup.cs
+++ checkers_bot/checkers_bot/Startup.cs
@@ -11,7 +11,6 @@ namespace checkers_bot
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-            
         }
 
         public IConfiguration Configuration { get; }
